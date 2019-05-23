95fc410d77fec77d7cd48c9e8f787f7cfae43c10
Jesús Rubio <jesusprubio@gmail.com>
2019-05-23 23:31:39 +0200

    Minor improvement in tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -10,19 +10,17 @@ extern crate online;
 extern crate pretty_assertions;
 
 #[cfg(test)]
-mod am {
-    use online::*;
-    use std::time::Duration;
+use online::*;
+use std::time::Duration;
 
-    #[test]
-    fn should_work_no_parameters() {
-        assert_eq!(online(None), Ok(true));
-    }
+#[test]
+fn should_work_no_parameters() {
+    assert_eq!(online(None), Ok(true));
+}
 
-    #[test]
-    fn should_work_timeout() {
-        let timeout = Duration::new(6, 0);
+#[test]
+fn should_work_timeout() {
+    let timeout = Duration::new(6, 0);
 
-        assert_eq!(online(Some(timeout)), Ok(true));
-    }
+    assert_eq!(online(Some(timeout)), Ok(true));
 }
