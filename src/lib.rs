e9ca45ed179c38d87056a88136a0b88cb7cb7c3d
Jesus Rubio <jesusprubio@gmail.com>
2021-01-13 00:21:35 +0100

    Drop license from files

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,3 @@
-/**
- * Copyright (c) 2019, Jesús Rubio <jesusprubio@gmail.com>
- *
- * This source code is licensed under the MIT License found in
- * the LICENSE.txt file in the root directory of this source tree.
- */
 use async_std::{
     io::{timeout as tout, Error},
     net::TcpStream,
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2019, Jesús Rubio <jesusprubio@gmail.com>
- *
- * This source code is licensed under the MIT License found in
- * the LICENSE.txt file in the root directory of this source tree.
- */
-
 #[cfg(test)]
 use online::*;
 use pretty_assertions::assert_eq;
